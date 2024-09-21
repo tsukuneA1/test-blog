9f766c7ef3154ea48cb7030422f0e3732935bc52
SaeYoshizaki <yoshizakisae@gmail.com>
2024-09-21 18:04:50 +0900

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
+
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://SaeYoshizaki.github.io/',
-  outDir: './docs',
-  base: '/wincblog_wordpress',
   integrations: [react()]
 });
-
