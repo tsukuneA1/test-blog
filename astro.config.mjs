8ee1142630c20f6ae238c6c49976093944229ce9
A1 <tkschr1125@gmail.com>
2024-09-22 14:24:31 +0900

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import react from '@astrojs/react';
 
 import vercel from '@astrojs/vercel/serverless';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: 'server',
-  adapter: vercel()
+  adapter: netlify()
 });
